57681cddfa37ad238c0f9555ecfc82a1255b9c5f
AnticuchoNotCucho <jorg.munozm@duocuc.cl>
2022-06-10 19:31:47 -0400

    Creada funcion obtener gps

--- AnticuchoTests/app/src/main/java/com/example/appmobile/MainActivity.java
+++ AnticuchoTests/app/src/main/java/com/example/appmobile/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.appmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private String stringLatitude = "0.0";
+    private String stringLongitude = "0.0";
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED); // Request permission
+        textView = findViewById(R.id.textView);
+    }
+
+    public void buttonGetLocation(View view) {
+        // Do something in response to button click
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    stringLatitude = Double.toString(location.getLatitude());
+                    stringLongitude = Double.toString(location.getLongitude());
+                    textView.setText("Latitude: " + stringLatitude + "\nLongitude: " + stringLongitude);
+
+            }
+        }
+
+        });
+
+        textView.setText("Latitude: " + stringLatitude + "\nLongitude: " + stringLongitude);
+
     }
 }
